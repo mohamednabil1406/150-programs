a878a85082496a29b8fedabbb0f838da226cf32c
mohamednabil1406 <connectwithmhd@gmail.com>
2025-08-16 14:52:44 +0530

    new

---
+++ array/Find Second largest in Array.py
@@ -0,0 +1,17 @@
+#second largest
+
+def second_large(arr):
+    first=second=-99999999
+
+    for num in arr:
+        if num>first:
+            second=first
+            first=num
+        elif num>second and num!=first:
+            second=num
+
+    if second==-99999999:
+            return None
+    return second
+arr=[12,35,1,10,34,1]
+print("the second laregst",second_large(arr))
